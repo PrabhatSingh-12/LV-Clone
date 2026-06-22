7d1b631f5019b9f09f65202c496e5c6066da4dbc
PrabhatSingh-12 <prahat55singh@gmail.com>
2026-06-23 00:11:02 +0530

    louisVuitton Clone

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-100px";
+    } else {
+        navbar.style.top = "0";
+        if (scrollTop === 0) {
+            navbar.classList.remove("scrolled");
+        } else {
+            navbar.classList.add("scrolled");
+        }
+    }
+    lastScrollTop = scrollTop;
+}
+);
+
+
+
+//Accordian
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        var panel = this.nextElementSibling;
+        var isActive = this.classList.contains("active");
+
+        // Close all accordion panels
+
+        for (var j = 0; j < acc.length; j++) {
+            var otherPanel = acc[j].nextElementSibling;
+            if (otherPanel !== panel) {
+                otherPanel.style.maxHeight = null;
+                acc[j].classList.remove("active");
+            }
+        }
+
+        if (isActive) {
+            this.classList.remove("active");
+            panel.style.maxHeight = null;
+        } else {
+            this.classList.add("active");
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+    );
+}
+
